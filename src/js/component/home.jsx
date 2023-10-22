f25dca16d72ec702633c063b22defa6bafbb6d1c
Liliana Aqueveque <130264861+liliaqvq@users.noreply.github.com>
2023-10-22 17:22:35 +0000

    added raw instructions

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -24,3 +24,20 @@ const Home = () => {
 };
 
 export default Home;
+
+//Instrucciones:
+//The light has to glow when clicked.
+/*
+The whole purpose of the component is to display a traffic light with red, yellow and green lights.
+When any light is clicked (selected) it has to glow, but the other lights have to stop glowing.
+The component must have a hooked state variable that tracks the color:
+
+const [ color, setColor] = useState("red");
+
+Use the setColor function to change the color and the component will automatically re-render (because it's hooked with useState).
+
+Use the ReactDOM.render to render the component into the DOM like this
+
+ReactDOM.render(<TrafficLight />, document.querySelector('#app'));
+
+*/
